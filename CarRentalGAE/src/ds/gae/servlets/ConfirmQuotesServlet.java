7cfe8ed0bfd466b364e821d7220fd64d20d850af
Joren Verspeelt <joren.verspeelt@gmail.com>
2015-12-01 11:44:40 +0100

    pushie pushie

--- CarRentalGAE/src/ds/gae/servlets/ConfirmQuotesServlet.java
+++ CarRentalGAE/src/ds/gae/servlets/ConfirmQuotesServlet.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import ds.gae.CarRentalModel;
-import ds.gae.ReservationException;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
 import ds.gae.entities.Quote;
-import ds.gae.view.ViewTools;
 import ds.gae.view.JSPSite;
 
 @SuppressWarnings("serial")
@@ -25,26 +27,30 @@ public class ConfirmQuotesServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		HttpSession session = req.getSession();
+		
+		String key = session.getId();
 		HashMap<String, ArrayList<Quote>> allQuotes = (HashMap<String, ArrayList<Quote>>) session.getAttribute("quotes");
 
-		try {
-			ArrayList<Quote> qs = new ArrayList<Quote>();
-			
-			for (String crcName : allQuotes.keySet()) {
-				qs.addAll(allQuotes.get(crcName));
-			}
-			CarRentalModel.get().confirmQuotes(qs);
-			
-			session.setAttribute("quotes", new HashMap<String, ArrayList<Quote>>());
-			
-			// TODO
-			// If you wish confirmQuotesReply.jsp to be shown to the client as
-			// a response of calling this servlet, please replace the following line 
-			// with resp.sendRedirect(JSPSite.CONFIRM_QUOTES_RESPONSE.url());
-			resp.sendRedirect(JSPSite.CREATE_QUOTES.url());
-		} catch (ReservationException e) {
-			session.setAttribute("errorMsg", ViewTools.encodeHTML(e.getMessage()));
-			resp.sendRedirect(JSPSite.RESERVATION_ERROR.url());				
+		ArrayList<Quote> quotes = new ArrayList<Quote>();
+		
+		for (String crcName : allQuotes.keySet()) {
+			quotes.addAll(allQuotes.get(crcName));
 		}
+		
+		String load = new Gson().toJson(quotes);
+		String sessionId = req.getSession().getId();
+		String username = (String) req.getSession().getAttribute("renter");
+		Queue defaultQueue = QueueFactory.getDefaultQueue();
+		defaultQueue.add(TaskOptions.Builder.withUrl("/worker").param("key", key)
+				.param("payload", load).param("ck", sessionId+username).param("renter", username));
+		
+		
+		session.setAttribute("quotes", new HashMap<String, ArrayList<Quote>>());
+		
+		// TODO
+		// If you wish confirmQuotesReply.jsp to be shown to the client as
+		// a response of calling this servlet, please replace the following line 
+		// with resp.sendRedirect(JSPSite.CONFIRM_QUOTES_RESPONSE.url());
+		resp.sendRedirect(JSPSite.CREATE_QUOTES.url());
 	}
 }
